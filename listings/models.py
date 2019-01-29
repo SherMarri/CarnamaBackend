c991d7647a4ebfa9909f83d5a5d957f20d1b2677
SherMarri <marribaloch128@gmail.com>
2019-01-30 01:39:53 +0500

    Updated Ad model

--- listings/models.py
+++ listings/models.py
@@ -1,10 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 from common.models import City, BaseModel
 from vehicles.models import Variant, Feature
 
+User = get_user_model()
+
 
 class Ad(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     variant = models.ForeignKey(Variant, on_delete=models.CASCADE)
     year = models.IntegerField()
     color = models.CharField(max_length=20)
