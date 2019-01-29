5374b94197d310bb725d01b88763ea7d7812b11f
SherMarri <marribaloch128@gmail.com>
2019-01-30 03:50:09 +0500

    Added serializers and views for vehicle app models

--- vehicles/views.py
+++ vehicles/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAdminUser
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from vehicles import serializers, models
+
+
+class MakeViewSet(ModelViewSet):
+    permission_classes = (IsAdminUser,)
+    serializer_class = serializers.MakeSerializer
+    queryset = models.Make.objects.all()
+
+
+class ModelViewSet(ModelViewSet):
+    permission_classes = (IsAdminUser,)
+    serializer_class = serializers.ModelSerializer
+    queryset = models.Model.objects.all()
+
+
+class VariantViewSet(ModelViewSet):
+    permission_classes = (IsAdminUser,)
+    serializer_class = serializers.VariantSerializer
+    queryset = models.Variant.objects.all()
+
+
+class FeatureViewSet(ModelViewSet):
+    permission_classes = (IsAdminUser,)
+    serializer_class = serializers.FeatureSerializer
+    queryset = models.Feature.objects.all()
