10de3426f9929e0f5b1462284c117380f3059084
Falak Sher Marri <falak.marri@visualsparks.com>
2019-04-04 17:32:42 +0500

    Added management command for adding features

---
+++ vehicles/management/populate_features.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+import csv
+from vehicles import models
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        m = []
+        with open('Features.csv') as file:
+            reader = csv.reader(file, delimiter=',')
+            for row in reader:
+                m.append(models.Feature(
+                    name=row[1], vehicle_type=models.CAR, code=row[-1]))
+        models.Feature.objects.bulk_create(m)
+        print('{0} features added.'.format(len(m)))
+
+
